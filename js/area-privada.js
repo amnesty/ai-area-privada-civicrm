c8e2aa5a7b0c41cbed72c465fde71295bc1f5893
Helena <hgarcia@es.amnesty.org>
2018-02-02 11:10:47 +0100

    Añadido cálculo de cuotas sugeridas

--- js/area-privada.js
+++ js/area-privada.js
@@ -13,119 +13,134 @@ jQuery(function($) {
 
     if( $('.webform-client-form').hasClass('webform-conditional-processed') ){
 
-        $(".content-datos-acceso").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+        if( $(".content-datos-acceso").length > 0 ){
+          $(".content-datos-acceso").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+          $(".content-datos-acceso").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-datos-acceso").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+        }
 
-        });
+        if( $(".content-colaborar").length > 0 ){
+          $(".content-colaborar").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-colaborar").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+          $(".content-colaborar").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+        }
 
-        $(".content-colaborar").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+        if( $(".content-datos").length > 0 ){
+          $(".content-datos").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+          $(".content-datos").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-datos").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+        }
 
-        });
+        if( $(".content-direccion").length > 0 ){
+          $(".content-direccion").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-datos").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+          $(".content-direccion").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
+          });
+        }
 
-        });
+        if( $(".content-cuenta").length > 0 ){
+          $(".content-cuenta").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-direccion").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+          $(".content-cuenta").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+        }
 
-        $(".content-direccion").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-        });
+        if( $(".content-idioma").length > 0 ){
+          $(".content-idioma").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-cuenta").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+          $(".content-idioma").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+        }
 
-        $(".content-cuenta").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+        if( $(".content-donativo").length > 0 ){
+          $(".content-donativo").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+          $(".content-donativo").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-idioma").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+        }
 
-        });
-        $(".content-idioma").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+        if( $(".content-certificado").length > 0 ){
+          $(".content-certificado").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+          $(".content-certificado").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-donativo").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+        }
 
-        });
-        $(".content-donativo").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+        if( $(".content-historial").length > 0 ){
+          $(".content-historial").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+          $(".content-historial").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-certificado").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+          });
+        }
 
-        });
-        $(".content-certificado").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+        if( $(".content-preferencias").length > 0 ){
+          $(".content-preferencias").hover( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        });
+          });
+          $(".content-preferencias").keyup( function(){
+              $(".caja-content").removeClass('active');
+              $(this).addClass('active');
 
-        $(".content-historial").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
-        $(".content-historial").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
-
-        $(".content-preferencias").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
-        $(".content-preferencias").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
+          });
+        }
 
     }
     else {
@@ -134,15 +149,17 @@ jQuery(function($) {
 
     // Sort the countries
     $( document ).ready(function() {
-      var options = $('.pais option');
-      var selected = $('.pais option:selected').val();
-      var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
-      arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
-      options.each(function(i, o) {
-        o.value = arr[i].v;
-        $(o).text(arr[i].t);
-      });
-      $(".pais").val(selected);
+      if( $(".pais").length > 0 ){
+          var options = $('.pais option');
+          var selected = $('.pais option:selected').val();
+          var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
+          arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
+          options.each(function(i, o) {
+            o.value = arr[i].v;
+            $(o).text(arr[i].t);
+          });
+          $(".pais").val(selected);
+      }
     });
 
     // Province label
@@ -157,33 +174,33 @@ jQuery(function($) {
     }
 
     // Make the IBAN fields to automatically move the cursor through when any field is fullfilled.
+    if( $(".country").length > 0 ){
+        $(".country").keyup( function(){
+          if($(".country").val().length >= 2){
+              $(".entity").focus();
+          }
+        });
 
-    $(".country").keyup( function(){
-      if($(".country").val().length >= 2){
-          $(".entity").focus();
-      }
-    });
+        $(".entity").keyup( function(){
+          if($(".entity").val().length >= 4){
+              $(".office").focus();
+          }
+        });
 
-    $(".entity").keyup( function(){
-      if($(".entity").val().length >= 4){
-          $(".office").focus();
-      }
-    });
+        $(".office").keyup( function(){
+          if($(".office").val().length >= 4){
+              $(".check").focus();
+          }
+        });
 
-    $(".office").keyup( function(){
-      if($(".office").val().length >= 4){
-          $(".check").focus();
-      }
-    });
-
-    $(".check").keyup( function(){
-      if($(".check").val().length >= 2){
-          $(".account").focus();
-      }
-    });
+        $(".check").keyup( function(){
+          if($(".check").val().length >= 2){
+              $(".account").focus();
+          }
+        });
+    }
 
     // Mark errors in select boxes
-
     $(".error").not(".messages").each( function(){
       if ($(this).is("select")){
           $(this).parent().addClass("form-error");
@@ -203,8 +220,10 @@ jQuery(function($) {
     }
 
     // Popups
-    $('a.popup').colorbox({iframe:true, width:"50%", height:"50%"});
-    $('a.popup_little').colorbox({iframe:true, width:"50%", height:"20%", "scrolling":false});
+    if( $("a.popup").length > 0 ){
+      $('a.popup').colorbox({iframe:true, width:"50%", height:"50%"});
+      $('a.popup_little').colorbox({iframe:true, width:"50%", height:"20%", "scrolling":false});
+    }
 
     $(document).bind('cbox_complete', function() {
         setTimeout(function () {
@@ -220,12 +239,14 @@ jQuery(function($) {
     }
 
     // 0 del DNI
-    $('.number-dni').first().focusout(function(){
-        var dni = String($(this).val());
-        if(dni.substring(0,1)=='0'){
-            $(this).val(dni.substring(1));
-        }
-    });
+    if( $(".number-dni").length > 0 ){
+      $('.number-dni').first().focusout(function(){
+          var dni = String($(this).val());
+          if(dni.substring(0,1)=='0'){
+              $(this).val(dni.substring(1));
+          }
+      });
+    }
 
     // Redes sociales
 
@@ -263,12 +284,13 @@ jQuery(function($) {
     });
 
     // ??????????
-    $('.webform-component--caja-preferncias-comunicacion--fila-1-preferencias--civicrm-1-contact-1-cg10-custom-26 label').removeClass('element-invisible');
+    //$('.webform-component--caja-preferncias-comunicacion--fila-1-preferencias--civicrm-1-contact-1-cg10-custom-26 label').removeClass('element-invisible');
 
+    // Bloques plegables
     attachVisibilityClassesToBoxes();
     function attachVisibilityClassesToBoxes() {
         var $boxes = $('.node-type-webform .webform-client-form .caja-content');
-        var bloqueDatosPersonalesIndex = 1;
+        var bloqueDatosPersonalesIndex = 0;
         $boxes.each(function(index) {
             var boxClass = index == bloqueDatosPersonalesIndex ? 'caja-visible' : 'caja-hidden';
             var $box = $(this);
@@ -306,11 +328,7 @@ jQuery(function($) {
         });
     }
 
-    // Borrar campos de nueva contraseña
-    $( document ).ready(function() {
-        $(".password").val("");
-    });
-
+    // Cambiar los símbolos con el despliegue de los bloques
     function masMenos() {
       // Botones de + y de -
       $(".mas").remove();
@@ -325,21 +343,44 @@ jQuery(function($) {
       });
     }
 
+    // Borrar campos de nueva contraseña
+    if( $(".password").length > 0 ){
+      $( document ).ready(function() {
+          $(".password").val("");
+      });
+    }
+
     // Mover texto del pie a la barra del boton
-    if( $(".webform-conditional-processed").length > 0 ){
+    //if( $(".webform-conditional-processed").length > 0 ){
       var submit = $(".form-actions");
       $(".footer").after(submit);
 
       $(".form-submit").click(function(){
           $(".webform-client-form").submit();
       });
+    //}
 
+    if( $(".cuota_actual").length > 0 ){
+
+      // Rellenar la cuota actual
+      var cuota_act = $(".cuota input:checked").val();
+      var period_act = $(".frecuencia option:selected").text();
+      $(".cuota_actual").val(cuota_act+' €');
+      $(".period_actual").val(period_act);
+
+      // Calcular cuotas sugeridas x 1.2, 1.5 y 2
+      var cuota_uno_dos = cuota_act*1.2;
+      $(".cuotas div div:first-child input").val(cuota_uno_dos);
+      $(".cuotas div div:first-child label").text(cuota_uno_dos+"€");
+
+      var cuota_uno_cinco = cuota_act*1.5;
+      $(".cuotas div div:nth-child(2) input").val(cuota_uno_cinco);
+      $(".cuotas div div:nth-child(2) label").text(cuota_uno_cinco+"€");
+
+      var cuota_dos = cuota_act*2;
+      $(".cuotas div div:nth-child(3) input").val(cuota_dos);
+      $(".cuotas div div:nth-child(3) label").text(cuota_dos+"€");
     }
 
-    // Rellenar la cuota actual
-    var cuota_act = $(".cuota input:checked").val()+' €';
-    var period_act = $(".frecuencia option:selected").text();
-    $(".cuota_actual").val(cuota_act);
-    $(".period_actual").val(period_act);
 
 })
