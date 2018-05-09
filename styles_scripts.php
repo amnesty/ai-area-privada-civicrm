5b1ed3f70f766a6881808127545d0280a70368c5
Helena <hgarcia@es.amnesty.org>
2018-05-09 13:23:10 +0200

    incluida la libreria jquery temporalmente para que funcione la pagina de descarga de certificado

--- styles_scripts.php
+++ styles_scripts.php
@@ -1,7 +1,7 @@
 <!-- *************** CSS y JS ****************** -->
 
   <!-- jQuery Core libraries -->
-  <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
