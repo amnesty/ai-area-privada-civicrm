f07c83a2bce79800608a184a1b83c63582ea0aea
Helena <hgarcia@es.amnesty.org>
2018-05-09 13:35:26 +0200

    añadido fichero provinces para traduccion de provincia en el certificado irpf

---
+++ provinces.php
@@ -0,0 +1,58 @@
+<?php
+
+$provinces = array();
+
+$provinces['VI']='Araba';
+$provinces['AB']='Albacete';
+$provinces['A']='Alicante';
+$provinces['AL']='Almería';
+$provinces['O']='Asturias';
+$provinces['AV']='Ávila';
+$provinces['BA']='Badajoz';
+$provinces['PM']='Baleares';
+$provinces['B']='Barcelona';
+$provinces['BU']='Burgos';
+$provinces['CC']='Cáceres';
+$provinces['CA']='Cádiz';
+$provinces['S']='Cantabria';
+$provinces['CS']='Castellón';
+$provinces['CR']='Ciudad Real';
+$provinces['CU']='Cuenca';
+$provinces['GE']='Girona';
+$provinces['GR']='Granada';
+$provinces['GU']='Guadalajara';
+$provinces['SS']='Gipuzkoa';
+$provinces['H']='Huelva';
+$provinces['HU']='Huesca';
+$provinces['J']='Jaén';
+$provinces['C']='La Coruña';
+$provinces['LO']='La Rioja';
+$provinces['GC']='Las Palmas';
+$provinces['LE']='León';
+$provinces['L']='Lleida';
+$provinces['LU']='Lugo';
+$provinces['M']='Madrid';
+$provinces['MA']='Málaga';
+$provinces['MU']='Murcia';
+$provinces['NA']='Navarra/Nafarroa';
+$provinces['OR']='Ourense';
+$provinces['P']='Palencia';
+$provinces['PO']='Pontevedra';
+$provinces['TF']='Santa Cruz de Tenerife';
+$provinces['SA']='Salamanca';
+$provinces['SG']='Segovia';
+$provinces['SE']='Sevilla';
+$provinces['SO']='Soria';
+$provinces['T']='Tarragona';
+$provinces['TE']='Teruel';
+$provinces['V']='Valencia';
+$provinces['VA']='Valladolid';
+$provinces['BI']='Bizkaia';
+$provinces['ZA']='Zamora';
+$provinces['Z']='Zaragoza';
+$provinces['CE']='Ceuta';
+$provinces['ML']='Melilla';
+$provinces['TO']='Toledo';
+$provinces['CO']='Córdoba';
+
+?>
