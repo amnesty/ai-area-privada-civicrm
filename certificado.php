151532f798e32c77fa77f400dafc4118400b7212
frodriguez <frodriguez@es.amnesty.org>
2024-05-07 13:47:09 +0200

    file_get_contents con variable DIR

--- certificado.php
+++ certificado.php
@@ -1,13 +1,14 @@
 <?php
-
+ 
   // libreria de conversion HTML => PDD
   require_once('lib/html2pdf/html2pdf.class.php');
   $meses = array('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
   $fecha = date ("j")." de ".$meses[date("n")-1]." de ".date ("Y")  ;
-
   // Montar el HTML
   global $base_url;
-  $certificado = file_get_contents($base_url . "/sites/all/themes/ai-area-privada-civicrm/plantilla_certificado.html");
+ //  $certificado = file_get_contents($base_url . "/sites/all/themes/ai-area-privada-civicrm/plantilla_certificado.html");
+  $certificado = file_get_contents(__DIR__."/plantilla_certificado.html"); 
+
   $certificado = str_replace("@destinatariofiscal@",$destinatario, $certificado);
   $certificado = str_replace("@dni@",$dni,$certificado);
   $certificado = str_replace("@domiciliofiscal@", htmlentities( trim($domicilio), ENT_QUOTES, "UTF-8" ), $certificado);
@@ -18,6 +19,7 @@
   $certificado = str_replace("@importenumero@",$importe,$certificado);
   $certificado = str_replace("@fecha@",$fecha,$certificado);
   $certificado = str_replace("@anyo@",$anyo,$certificado);
+
 
   ob_start();
   echo $certificado;
